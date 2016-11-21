659332e44eff714f08a0275c112fb843bbdde01b
zdy <dadayy1@outlook.com>
2016-11-21 17:40:20 +0800

    新闻列表-数据库

---
+++ SchoolPortal/SchoolPortal/Controllers/NewsController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SchoolPortal.Controllers
+{
+    public class NewsController : Controller
+    {
+        // GET: News
+        public ActionResult Index()
+        {
+            var db = new NewsDatabase();
+            db.Database.CreateIfNotExists();//创建数据库
+
+            var lst = db.Informations.OrderByDescending(o => o.Id).ToList(); //进行降序排序
+            ViewBag.Information = lst;
+
+            return View();
+        }
+    }
+}
