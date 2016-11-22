9416484f75b6d2079fc73016e46609b72b4c639e
zdy <dadayy1@outlook.com>
2016-11-22 17:27:38 +0800

    后台新闻管理页面

---
+++ SchoolPortal/SchoolPortal/Controllers/AdminController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SchoolPortal.Controllers
+{
+    public class AdminController : Controller
+    {
+        // GET: Admin
+        public ActionResult Index()
+        {
+            var db = new NewsDatabase();
+            db.Database.CreateIfNotExists();//创建数据库
+            var lst = db.Informations.AsQueryable();
+
+            //不同种类的新闻列表
+            var lst1 = lst.Where(o => o.Type.Contains("通知公告")); 
+            var lst2 = lst.Where(o => o.Type.Contains("科教动态")); 
+            var lst3 = lst.Where(o => o.Type.Contains("综合新闻"));
+
+            //进行降序排序
+            ViewBag.Informations1 = lst1.OrderByDescending(o => o.Id).ToList(); 
+            ViewBag.Informations2 = lst2.OrderByDescending(o => o.Id).ToList();
+            ViewBag.Informations3 = lst3.OrderByDescending(o => o.Id).ToList();
+
+
+            return View();
+        }
+    }
+}
